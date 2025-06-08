0d26516ade4287ff0a5fb6d1e5b366b19a8ed2b8
computergeek125 <computergeek125@gmail.com>
2025-06-08 02:41:11 +0000

    Upgrade GHA to support new CMK paths

--- .devcontainer/build.sh
+++ .devcontainer/build.sh
@@ -4,12 +4,12 @@ NAME=$(python -c 'print(eval(open("package").read())["name"])')
 rm /omd/sites/cmk/var/check_mk/packages/* ||:
 ln -s $WORKSPACE/package /omd/sites/cmk/var/check_mk/packages/$NAME
 
-mkp -v pack $NAME
+mkp -v package package
 
 # Set Outputs for GitHub Workflow steps
 if [ -n "$GITHUB_WORKSPACE" ]; then
-    echo "::set-output name=pkgfile::$(ls *.mkp)"
-    echo "::set-output name=pkgname::${NAME}"
+    echo "pkgfile=$(ls /omd/sites/cmk/local/share/check_mk/enabled_packages/*.mkp)" >> $GITHUB_OUTPUT
+    echo "pkgname=${NAME}" >> $GITHUB_OUTPUT
     VERSION=$(python -c 'print(eval(open("package").read())["version"])')
-    echo "::set-output name=pkgversion::$VERSION"
+    echo "pkgversion=$VERSION" >> $GITHUB_OUTPUT
 fi
